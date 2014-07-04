b0360899c53e9440960cd5202b2448931394583f
ixueping <ixueping@126.com>
2014-07-04 10:08:23 +0800

    删除测试查询样本户文件

--- src/cn/voicet/ybh/dao/YbhQueryDao.java
+++
@@ -1,8 +0,0 @@
-package cn.voicet.ybh.dao;
-
-import cn.voicet.ybh.util.DotSession;
-
-public interface YbhQueryDao extends BaseDao{
-	public final static String SERVICE_NAME = "cn.voicet.ybh.dao.impl.YbhQueryDaoImpl";
-	void getSelectedCunList(DotSession ds);
-}
